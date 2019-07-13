80ac49a7e3d3cf627ed1f4605916720eb7fca1e0
wowufoundme <shubhkirti.work@gmail.com>
2019-07-13 20:50:02 +0530

    Nav changes

--- compiled/script.js
+++ compiled/script.js
@@ -6,6 +6,38 @@ $(document).ready(function(){
   $('.popup-close').click(function(){
     $('#popup').addClass('popup-closed');
   });
+
+  // COLOR VARIABLES
+  BLUE = "#4285f4"
+  YELLOW = "#f4b400"
+  GREEN = "#0f9d58"
+  RED = "#db4437"
+  BLACK = "#0a0a0a"
+
+  // $('#home').hover(function(){
+  //   $('.nav-container').css('background-color', BLUE);
+  // }, function(){
+  //   $('.nav-container').css('background-color', BLACK);
+  // });
+
+  // $('#schedule').hover(function(){
+  //   $('.nav-container').css('background-color', YELLOW);
+  // }, function(){
+  //   $('.nav-container').css('background-color', BLACK);
+  // });
+
+  // $('#team').hover(function(){
+  //   $('.nav-container').css('background-color', GREEN);
+  // }, function(){
+  //   $('.nav-container').css('background-color', BLACK);
+  // });
+
+  // $('#contact').hover(function(){
+  //   $('.nav-container').css('background-color', RED);
+  // }, function(){
+  //   $('.nav-container').css('background-color', BLACK);
+  // });
+
 });
 $(window).on('load',function(){
   $('.intro').toggleClass('show-head');
